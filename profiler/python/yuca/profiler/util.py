9822731433e9c43b8211eb78c3e1c766350d35d9
atpoverload <atpoverload@gmail.com>
2025-08-16 19:37:20 -0600

    sd;fakew

--- profiler/python/yuca/profiler/util.py
+++ profiler/python/yuca/profiler/util.py
@@ -83,7 +83,6 @@ METRICS = [
 def process(profile, metrics, pbar=None):
     records = []
     period = None
-    print(profile.session)
     if profile.session.HasField('period'):
         period = 1000 * profile.session.period.secs + \
             profile.session.period.nanos // 1000000
@@ -99,10 +98,9 @@ def process(profile, metrics, pbar=None):
                 period = f'{period}us'
         else:
             period = f'{period}ms'
-    print(profile.session.metadata)
     metadata = dict((m.key, m.value) for m in profile.session.metadata)
     metadata['period'] = period
-    print(profile.session.metadata)
+    print(metadata)
     if pbar is not None:
         pbar.set_description(
             f'period: {period}', list(map(lambda m: f'{m[0]}: {m[1]}', metadata.items())))
