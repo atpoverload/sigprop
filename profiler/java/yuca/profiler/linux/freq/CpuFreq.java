dbc507f9051efe99c51fddc8f071d086c1641608
atpoverload <atpoverload@gmail.com>
2025-08-26 15:35:51 -0600

    what the hell is happening

--- profiler/java/yuca/profiler/linux/freq/CpuFreq.java
+++ profiler/java/yuca/profiler/linux/freq/CpuFreq.java
@@ -63,6 +63,7 @@ public final class CpuFreq {
 
   private static int readCounter(int cpu, String component) {
     String counter = readFromComponent(cpu, component);
+    System.out.println(String.format("%s: %s", component, counter));
     if (counter.isBlank()) {
       return 0;
     }
